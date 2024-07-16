c52ff8af3312a8cf90d30adf893b4c9173c646ca
Rachel Peterson <rachel.peterson.5683@gmail.com>
2024-07-15 23:39:38 -0600

    updated styling on homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -10,7 +10,9 @@ function Homepage() {
     <img src={mountainphoto} id="mountain-photo"></img>
 
     <h1 className="intro-text-1">HI, I'M</h1>
-      <h1 className="intro-text-2">RACHEL.</h1>
+      <h1 className="intro-text-2">RACHEL</h1>      
+      <h1 className="intro-text-3">.</h1>
+
       <img src={profilephoto} id="profile-photo"></img>
       <p id="short-bio">I'm a recent computer science graduate, committed to using technology as a force for positive change.</p>
       </div>
