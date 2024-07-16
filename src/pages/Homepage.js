5662b1ed5704908b59dce4a0525e1063417666be
Rachel Peterson <rachel.peterson.5683@gmail.com>
2024-07-16 10:00:58 -0600

    font color update

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -14,7 +14,7 @@ function Homepage() {
       <h1 className="text-[6vw] text-left absolute pl-[38.5%] pt-[5%] animate-fadeSlow">.</h1>
 
       <img src={profilephoto} className="w-[35vw] h-auto absolute ml-[50%] pt-[20%] animate-fadeFast"></img>
-      <p className="text-[2vw] w-[50vw] font-extralight absolute ml-[2vw] pl-[5%] pt-[15%] pb-[50px] animate-fadeSlowest">I'm a recent computer science graduate, committed to using technology as a force for positive change.</p>
+      <p className="text-[2vw] w-[50vw] font-extralight absolute ml-[2vw] pl-[5%] pt-[15%] pb-[50px] animate-fadeSlowest text-stone-400">I'm a recent computer science graduate, committed to using technology as a force for positive change.</p>
       </div>
 
       
